11e7b6126f3bacf1d8ef3c9a306868f3b3f8b5fa
madelaine <trujillo.maddie11@gmail.com>
2021-01-14 15:57:51 -0800

    dummy copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,15 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Black History Month Landing Page" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="BHM Landing Page" />
+    <h1>Black History Month</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nisl purus in mollis nunc sed. Mollis aliquam ut porttitor leo a diam. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas.</p>
+    <p>Cursus turpis massa tincidunt dui ut. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Quisque id diam vel quam elementum pulvinar. Phasellus vestibulum lorem sed risus ultricies tristique. Praesent semper feugiat nibh sed. At imperdiet dui accumsan sit amet.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
   </Layout>
 )
 
